92435e1d277410794c15833acc350faf26d7ca24
chhagan kumawat <122602115+chhagan72@users.noreply.github.com>
2026-06-13 18:32:03 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
-
-// Map clean sub-routes directly to their controllers
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
